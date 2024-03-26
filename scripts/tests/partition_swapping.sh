a9ff44c5dfec00e80ea18d64d5dce61ae599627e
DavidHidde <dahibo@kpnmail.nl>
2024-03-26 16:12:34 +0100

    Also add repetitions to partition_swapping

--- scripts/tests/partition_swapping.sh
+++ scripts/tests/partition_swapping.sh
@@ -2,29 +2,17 @@
 executable=$1 # First arg: The executable location
 output_dir=$2 # Second arg: The output directory
 
-base="$executable --debug --rows 256 --columns 256"
+base="$executable --debug --rows 256 --columns 256 --seed 1"
 
-echo "########### Aggregate Hierarchy ###########"
-$base --targets 0 --seed 1 --partition_swaps=true --output "$output_dir/aggregate_hierarchy/true/"
-echo "---"
-$base --targets 0 --seed 1 --partition_swaps=false --output "$output_dir/aggregate_hierarchy/false/"
+target_names=( "aggregate_hierarchy" "highest_parent" "aggregate_hierarchy_4c" "highest_parent_4c" "partition_neighbourhood" )
 
-echo "########### Highest parent ###########"
-$base --targets 1 --seed 1 --partition_swaps=true --output "$output_dir/highest_parent/true/"
-echo "---"
-$base --targets 1 --seed 1 --partition_swaps=false --output "$output_dir/highest_parent/false/"
-
-echo "########### Aggregate Hierarchy 4c ###########"
-$base --targets 2 --seed 1 --partition_swaps=true --output "$output_dir/aggregate_hierarchy_4c/true/"
-echo "---"
-$base --targets 2 --seed 1 --partition_swaps=false --output "$output_dir/aggregate_hierarchy_4c/false/"
-
-echo "########### Highest parent 4c ###########"
-$base --targets 3 --seed 1 --partition_swaps=true --output "$output_dir/highest_parent_4c/true/"
-echo "---"
-$base --targets 3 --seed 1 --partition_swaps=false --output "$output_dir/highest_parent_4c/false/"
-
-echo "########### Partition neighbourhood ###########"
-$base --targets 4 --seed 1 --partition_swaps=true --output "$output_dir/partition_neighbourhood/true/"
-echo "---"
-$base --targets 4 --seed 1 --partition_swaps=false --output "$output_dir/partition_neighbourhood/false/"
+# Iterate over all possible combinations
+for target_idx in {0..4}; do
+  echo "########### ${target_names[$target_idx]} ###########"
+  for repetition in {0..4}; do
+        current_output_dir="$output_dir/${target_names[$target_idx]}/true/$repetition/"
+        $base --targets "$target_idx" --partition_swaps=true --output "$current_output_dir"
+        current_output_dir="$output_dir/${target_names[$target_idx]}/false/$repetition/"
+        $base --targets "$target_idx" --partition_swaps=false --output "$current_output_dir"
+  done
+done
